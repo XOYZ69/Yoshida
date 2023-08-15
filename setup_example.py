1d7ff4f9b1159c9cf6707e5d9f44e9b724b14875
Kevin Pagenkämper <xoyz.productions@gmail.com>
2023-08-15 05:52:48 +0200

    Workflow Optimizations and Pytest advancements

--- setup_example.py
+++ setup_example.py
@@ -3,27 +3,58 @@
 import json
 import pytest
 import os
+import shutil
 
 from modules.Card import Card
 
-cards = []
+def test_card_creation_basis(card_set = ''):
 
-path = 'data/card_sets/example_game_cards'
+    print('Testing card set:', card_set)
 
-def test_card_creation():
+    save_location = 'data/output/{card_set}/'.format(card_set = card_set)
 
-    with open(path + '.json', 'r', encoding='utf-8') as example:
+    # Cleanup old files from testing
+    if os.path.exists(save_location):
+        shutil.rmtree(save_location)
+
+    os.makedirs(save_location, exist_ok = True)
+
+    cards = []
+
+    json_file = 'data/card_sets/{card_set}.json'.format(card_set = card_set)
+
+    if card_set == '':
+        print('Empty card set is detected correctly.')
+        assert True
+        return
+
+    assert os.path.exists(json_file)
+
+    with open(json_file, 'r', encoding = 'utf-8') as example:
         example_json = json.loads(example.read())
-        for card in example_json['cards']:
-            print('Card:', card)
-            cards.append(Card(example_json['design']))
-            cards[-1].create(card)
-        
-        # for card in cards:
-        #     card.show()
-        # cards[0].show()
-        cards[0].card_img.save(path + '_.png')
-    
-    assert os.path.exists(path + '_.png')
 
-test_card_creation()
+        cards.append(Card(example_json['design']))
+
+        card_count = 0
+
+        for card in example_json['cards']:
+            cards[-1].create(card)
+
+            card_count += 1
+
+            if 'var_id' in card:
+                filename = card['var_id'] + '.png'
+            else:
+                filename = str(card_count) + '.png'
+            
+            cards[-1].card_img.save(save_location + filename)
+
+            print('Card {card} completed.'.format(card = filename))
+
+            assert os.path.exists(save_location + filename), 'Generated file does not exist.'
+
+def test_gamecard_simple():
+    test_card_creation_basis('example_gamecard_simple')
+
+def test_yugioh():
+    test_card_creation_basis('example_yu-gi-oh')
